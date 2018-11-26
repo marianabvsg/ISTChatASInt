757d92e479f99b5063d7c90d73d90b6f9480e532
rena2henriques <renato.manao@gmail.com>
2018-11-26 16:34:02 +0000

    added a simple key generator for the bot

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+var apikey = require("apikeygen").apikey;
 
 // temporary variable
 const adminkey = "secretkey";
@@ -52,8 +53,39 @@ router.get('/list/logs/movements/:user', function(req, res) {
     
 })
 
+// Generate an api key for a new bot
 router.get('/bot', function(req, res) {
+    // the get request is supposed to receive the building of the bot
     
+    // FLOW OF THIS FUNCTION
+    // if the building exists
+    // generate unique api key
+    // save the key into the DB with the correspondent building
+    // return the key
+
+    const building = req.body.building;
+
+    // check authentication of admin
+    // TODO
+
+    // if the building exists
+    if(building == null /* or doesn't belong to the DB of buildings */) {
+
+        // not found
+        res.sendStatus(404);
+    } else {
+
+        var key = apikey();  // generates 40 char base64 encoded key
+
+        // save the key into the DB with the correspondent building
+        // TODO
+
+        // return the key to the admin
+        // mandar status 200????
+        res.send({
+            'key': key
+        })
+    }
 })
 
 
