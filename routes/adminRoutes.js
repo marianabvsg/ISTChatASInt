5359fa2232312400b45b5b975dbc4f637b71d00d
rena2henriques <renato.manao@gmail.com>
2018-11-26 16:14:42 +0000

    added a very stupid login mode for the admin

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,6 +1,33 @@
 const express = require('express')
 const router = express.Router()
 
+// temporary variable
+const adminkey = "secretkey";
+
+router.get('/login', function(req, res) {
+    
+    // ADD SECURITY LATER
+
+    if(req.body.username == 'admin' &&  req.body.password == '123') {
+        // the user is the admin
+        // authentication OK
+        
+        // return a key to the admin
+        res.send({
+            'adminkey': adminkey
+        })
+
+    } else {
+        // authentication NOK
+
+        // don't return nothing
+        res.send({
+            'adminkey': null
+        })
+    }
+
+})
+
 router.post('/building', function(req, res) {
     
 })
@@ -28,13 +55,6 @@ router.get('/list/logs/movements/:user', function(req, res) {
 router.get('/bot', function(req, res) {
     
 })
-
-router.get('/auth', function(req, res) {
-    
-    // TODO
-
-})
-
 
 
 module.exports = router;
