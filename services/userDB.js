d579e7d582039ae19a52c8b1d61a41d0bc74ea15
mariana <mariana.galrinho@tecnico.ulisboa.pt>
2018-11-28 23:50:08 +0000

    pseudo code for userRoutes

--- services/userDB.js
+++ services/userDB.js
@@ -22,8 +22,25 @@ module.exports = class userDB {
         //returns all users by building
     }
 
-    setRange(user, range) {
+    listUsersInRange(user){
+        // returns all users which are within the defined range for the input user
+    }
 
+    // checkUserIsClose(){
+    //     //por esta funcao ou a de cima?
+    // }
+
+    listUsersNearby(user){
+        //makes use of listUsersByBuilding and listUsersInRange? 
+        //ou então checka-se se para todos os que estão no building, se estão dentro do range 
+        // e nesse caso pode-se meter tipo uma função que checka se está dentro do range
+
+        // returns all users which are within the defined range and in the same building
+    }
+
+
+    setRange(user, range) {
+        //returns true if range set successfuly
     }
 
 }
