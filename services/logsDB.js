26500af2a523d6eebdc5fa3b57e67e3e29b44a48
rena2henriques <renato.manao@gmail.com>
2018-11-28 18:22:48 +0000

    added secret check to admins requests

---
+++ services/logsDB.js
@@ -0,0 +1,42 @@
+module.exports = class logsDB {
+
+    constructor(db) {
+        this.db = db
+    }
+
+    insertMessage(user_id, message) {
+        
+        //return
+    }
+
+    insertMove(user_id, building) {
+        
+        //return
+    }
+
+    listAll() {
+
+        
+    }
+
+    listByUser() {
+
+    }
+
+    listMovesByUser() {
+
+    }
+
+    listMessagesByUser() {
+
+    }
+
+    listByBuilding() {
+
+    }
+
+    listMessagesByBuilding() {
+
+    }
+
+}
